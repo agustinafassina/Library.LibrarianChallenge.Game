44ff38c6e6641bdffdcc19e9db2066aba3e98129
agustinafassina <agustinafassina@gmail.com>
2026-08-14 02:31:44 +0200

    fix: ignore vendored Phaser in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 
 export default [
   {
-    ignores: ["node_modules/**"],
+    ignores: ["node_modules/**", "vendor/**", "**/*.min.js"],
   },
   js.configs.recommended,
   {
